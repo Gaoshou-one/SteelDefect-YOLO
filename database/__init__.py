a8cda6b7ad41801dad2eae788a5ba309b16969fd
LI <129831612+Gaoshou-one@users.noreply.github.com>
2026-06-24 01:50:44 +0800

    Delete database directory

--- database/__init__.py
+++
@@ -1,5 +0,0 @@
-# database/__init__.py
-from .db_manager import DatabaseManager
-from .models import User, DetectionRecord, UserRole
-
-__all__ = ['DatabaseManager', 'User', 'DetectionRecord', 'UserRole']
